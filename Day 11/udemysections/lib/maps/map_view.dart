697b99c66834eb9aa05f55aa5f5167a1dbf89cb6
pratikjain04 <pratikjain0411@gmail.com>
2018-08-16 22:45:02 +0530

    Markers added

--- Day 11/udemysections/lib/maps/map_view.dart
+++ Day 11/udemysections/lib/maps/map_view.dart
@@ -2,19 +2,45 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong/latlong.dart';
 
+void main() => runApp(new MaterialApp(
+      home: new MyApp(),
+    ));
 
-void main() => runApp(new MaterialApp(home: new MyApp(),));
-
-class MyApp extends StatefulWidget{
-
+class MyApp extends StatefulWidget {
   @override
-  MyAppState createState()=> MyAppState();
+  MyAppState createState() => MyAppState();
 }
 
-class MyAppState extends State<MyApp>{
-
+class MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
+    var markers = <Marker>[
+      new Marker(
+          width: 80.0,
+          height: 80.0,
+          point: new LatLng(41.8781, -87.6298),
+          builder: (ctx) => new Icon(
+                Icons.pin_drop,
+                color: Colors.red,
+              )),
+      new Marker(
+          width: 80.0,
+          height: 80.0,
+          point: new LatLng(42.3314, -83.6298),
+          builder: (ctx) => new Icon(
+                Icons.pin_drop,
+                color: Colors.red,
+              )),
+      new Marker(
+          width: 80.0,
+          height: 80.0,
+          point: new LatLng(42.8781, -84.6298),
+          builder: (ctx) => new Icon(
+                Icons.pin_drop,
+                color: Colors.red,
+              )),
+    ];
+
     return new Scaffold(
       appBar: new AppBar(
         title: Center(child: new Text('Maps')),
@@ -26,17 +52,17 @@ class MyAppState extends State<MyApp>{
             children: <Widget>[
               new Flexible(
                   child: new FlutterMap(
-                    options: new MapOptions(
-                      center: new LatLng(41.8781, -87.6298),
-                      zoom: 15.0
-                    ),
+                options: new MapOptions(
+                    center: new LatLng(41.8781, -87.6298), zoom: 15.0),
                 layers: [
                   new TileLayerOptions(
-                  urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-                    subdomains: ['a','b','c'],
-                ), ]
-                  )
-              )
+                    urlTemplate:
+                        "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                    subdomains: ['a', 'b', 'c'],
+                  ),
+                  new MarkerLayerOptions(markers: markers)
+                ],
+              ))
             ],
           ),
         ),
